026dbcfb6ea5f2203c41788ff611d718152bae37
Arthur Henrique <arthurcostalima88@gmail.com>
2024-10-30 03:08:30 -0300

    error tag

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { titleSite } from './styles'
+
+const Header = () => (
+  <header>
+    <titleSite>
+      <h1 style={{ fontFamily: 'Sacramento, sans serif' }}>Signature</h1>
+      <h2 style={{ fontFamily: 'Big Shoulders Display, sans serif' }}>World</h2>
+    </titleSite>
+  </header>
+)
+
+export default Header
---
+++ src/Components/Header/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const titleSite = styled.div`
+  text-align: center;
+`
